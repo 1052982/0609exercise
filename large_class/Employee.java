29a9a6855bd3148d0681f5a1ad4bc939ba8c10d6
1052982 <nbanike5@gmail.com>
2019-05-25 15:41:14 +0800

    1052982.0

--- large_class/Employee.java
+++ large_class/Employee.java
@@ -3,26 +3,35 @@ package com.directi.training.codesmells.largeclass;
 public class Employee
 {
     private final String _name;
-    private final String _officeAreaCode;
-    private final String _officeNumber;
-    private final String _officeExtensionNumber;
+    private final String _TelephoneNumber;
 
-    public Employee(String name, String officeAreaCode, String officeNumber,
-                    String officeExtensionNumber)
+    public Employee(String name, Telephone TelephoneNumber)
     {
         _name = name;
-        _officeAreaCode = officeAreaCode;
-        _officeNumber = officeNumber;
-        _officeExtensionNumber = officeExtensionNumber;
+        _TelephoneNumber = TelephoneNumber;
     }
+	
+    public String getName()
+    {
+        return _name;
+    }
+}
+
+public class Telephone
+{
+    private final String _officeAreaCode;
+    private final String _officeNumber;
+    private final String _officeExtensionNumber;	
+    
+    public Telephone(String officeAreaCode,String officeNumber, 
+                     String officeExtensionNumber)
+    {
+       _officeAreaCode = officeAreaCode;
+       _officeNumber;= officeNumber;
+       _officeExtensionNumber = officeExtensionNumber;       
 
     public String getTelephoneNumber()
     {
         return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
-    }
-
-    public String getName()
-    {
-        return _name;
     }
 }
